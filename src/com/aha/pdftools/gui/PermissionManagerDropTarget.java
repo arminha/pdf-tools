b2f62a7b428c38bab9e16186125edf55203a3088
Armin Häberling <armin.aha@gmail.com>
2012-08-31 14:02:23 +0200

    Fix logger name.

--- src/com/aha/pdftools/gui/PermissionManagerDropTarget.java
+++ src/com/aha/pdftools/gui/PermissionManagerDropTarget.java
@@ -13,74 +13,72 @@ import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import com.aha.pdftools.PdfPermissionManagerGui;
-
 class PermissionManagerDropTarget extends DropTargetAdapter {
-    private static DataFlavor uriListFlavor = null;
-    static {
-        try {
-            uriListFlavor = new DataFlavor("text/uri-list; class=java.lang.String");
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(PdfPermissionManagerGui.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
-    private final PermissionManager manager;
-    
-    public PermissionManagerDropTarget(PermissionManager manager) {
+	private static DataFlavor uriListFlavor = null;
+	static {
+		try {
+			uriListFlavor = new DataFlavor("text/uri-list; class=java.lang.String");
+		} catch (ClassNotFoundException ex) {
+			Logger.getLogger(PermissionManagerDropTarget.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+
+	private final PermissionManager manager;
+
+	public PermissionManagerDropTarget(PermissionManager manager) {
 		this.manager = manager;
 	}
-    
-    private boolean openFiles(List<File> files) {
-    	List<File> finalFiles = new ArrayList<File>();
-    	for (File file : files) {
+
+	private boolean openFiles(List<File> files) {
+		List<File> finalFiles = new ArrayList<File>();
+		for (File file : files) {
 			if (!file.canRead()) {
 				continue;
 			}
 			if (file.isDirectory()) {
-	    		finalFiles.addAll(PermissionManager.filesInFolder(file));
-	    	} else {
-	    		finalFiles.add(file);
-	    	}
+				finalFiles.addAll(PermissionManager.filesInFolder(file));
+			} else {
+				finalFiles.add(file);
+			}
 		}
-    	
-    	if (finalFiles.isEmpty()) {
-    		return false;
-    	} else {
-    		manager.insertFiles(finalFiles);
-    		return true;
-    	}
-    }
-	
+
+		if (finalFiles.isEmpty()) {
+			return false;
+		} else {
+			manager.insertFiles(finalFiles);
+			return true;
+		}
+	}
+
 	@Override
 	public void drop(DropTargetDropEvent dtde) {
 		if (dtde.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
-            dtde.acceptDrop(DnDConstants.ACTION_COPY);
-            Transferable trans = dtde.getTransferable();
-            try {
-                @SuppressWarnings("unchecked")
+			dtde.acceptDrop(DnDConstants.ACTION_COPY);
+			Transferable trans = dtde.getTransferable();
+			try {
+				@SuppressWarnings("unchecked")
 				List<File> filelist = (List<File>) trans.getTransferData(DataFlavor.javaFileListFlavor);
-                dtde.dropComplete(openFiles(filelist));
-            } catch (Exception e) {
-                dtde.dropComplete(false);
-            }
-        } else if (dtde.isDataFlavorSupported(uriListFlavor)) {
-            dtde.acceptDrop(DnDConstants.ACTION_COPY);
-            Transferable trans = dtde.getTransferable();
-            try {
-                String uris = (String) trans.getTransferData(uriListFlavor);
-                StringTokenizer tokenizer = new StringTokenizer(uris);
-                List<File> files = new ArrayList<File>();
-                while (tokenizer.hasMoreTokens()) {
-                	URI uri = new URI(tokenizer.nextToken());
-                	files.add(new File(uri));
-                }
-                dtde.dropComplete(openFiles(files));
-            } catch (Exception e) {
-                dtde.dropComplete(false);
-            }
-        } else {
-            dtde.rejectDrop();
-        }
+				dtde.dropComplete(openFiles(filelist));
+			} catch (Exception e) {
+				dtde.dropComplete(false);
+			}
+		} else if (dtde.isDataFlavorSupported(uriListFlavor)) {
+			dtde.acceptDrop(DnDConstants.ACTION_COPY);
+			Transferable trans = dtde.getTransferable();
+			try {
+				String uris = (String) trans.getTransferData(uriListFlavor);
+				StringTokenizer tokenizer = new StringTokenizer(uris);
+				List<File> files = new ArrayList<File>();
+				while (tokenizer.hasMoreTokens()) {
+					URI uri = new URI(tokenizer.nextToken());
+					files.add(new File(uri));
+				}
+				dtde.dropComplete(openFiles(files));
+			} catch (Exception e) {
+				dtde.dropComplete(false);
+			}
+		} else {
+			dtde.rejectDrop();
+		}
 	}
 }
